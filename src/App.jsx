43cda31b6b294020553c4c249b5aae781e294d5d
florixak <florixakcz@seznam.cz>
2023-12-02 11:22:11 +0100

    React Essentials - finished ep. 19

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,30 @@
-import ReactConcepts from "./assets/react-core-concepts.png";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept/CoreConcept";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core", "Main"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
-    </li>
-  );
-}
-
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescriptions[genRandomInt(reactDescriptions.length - 1)]} React
-        concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
 
 function App() {
   return (
     <div>
       <Header />
-      <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          {CORE_CONCEPTS.map((item) => {
-            <CoreConcept image={item[0].image} title={item[0].title} description={item[0].description} />;
-          })}
-        </ul>
-      </section>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              image={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept
+              image={CORE_CONCEPTS[1].image}
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
