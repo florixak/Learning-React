55699724a8208a94b71445fb9962c6e67e32fac9
florixak <florixakcz@seznam.cz>
2023-12-08 15:55:43 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,12 +35,13 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
             <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-
         </section>
       </main>
     </div>
