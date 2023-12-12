8e9efc82f445077b29f3d83e846dadc7a666c3f5
florixak <florixakcz@seznam.cz>
2023-12-12 18:09:41 +0100

    Update

---
+++ src/components/CoreConcepts/CoreConcepts.jsx
@@ -0,0 +1,27 @@
+import "./CoreConcepts.css";
+
+import CoreConcept from "../CoreConcept/CoreConcept"
+import { CORE_CONCEPTS } from "../../data";
+
+const CoreConcepts = () => {
+  return (
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+
+        {/*CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept
+                title={conceptItem.title}
+                description={conceptItem.description}
+                image={conceptItem.image}
+              />
+            ))*/}
+      </ul>
+    </section>
+  );
+};
+
+export default CoreConcepts;
