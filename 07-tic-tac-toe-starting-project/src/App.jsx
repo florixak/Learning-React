30d04d7544617f9ec93a4ed9e048376425e48326
florixak <florixakcz@seznam.cz>
2024-01-03 10:40:18 +0100

    Update

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,6 +3,8 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
 
