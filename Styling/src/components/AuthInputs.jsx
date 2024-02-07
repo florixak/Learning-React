4d8f6e6e76709d5602c97be38fe2a3d20b13b5fc
florixak <florixakcz@seznam.cz>
2024-02-07 09:09:10 +0100

    Update

--- Styling/src/components/AuthInputs.jsx
+++ Styling/src/components/AuthInputs.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-
 import { styled } from "styled-components";
+
+import Button from "./Button";
 
 const StyledContainer = styled.div`
   display: flex;
@@ -9,26 +10,7 @@ const StyledContainer = styled.div`
   margin-bottom: 1.5rem;
 `;
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({$invalid}) => $invalid ? "#f87171" : "#6b7280"};
-`;
 
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({$invalid}) => $invalid ? "#fed2d2" : "#d1d5db"};
-  color: ${({$invalid}) => $invalid ? "#ef4444" : "#374151"};
-  border: 1px solid ${({$invalid}) => $invalid ? "#f73f3f" : "transparent"};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -65,9 +47,7 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <Label $invalid={passwordNotValid}>
-            Password
-          </Label>
+          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
             $invalid={passwordNotValid}
             type="password"
@@ -81,9 +61,9 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
+        <Button onClick={handleLogin}>
           Sign In
-        </button>
+        </Button>
       </div>
     </div>
   );
