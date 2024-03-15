157ea610f4706f6225e0f3329a2dfeb9569feaa4
florixak <florixakcz@seznam.cz>
2024-03-15 14:50:17 +0100

    Update

--- Challenges/src/components/ResultModal.jsx
+++ Challenges/src/components/ResultModal.jsx
@@ -1,6 +1,8 @@
-const ResultModal = ({ result, targetTime }) => {
+import { forwardRef } from "react";
+
+const ResultModal = forwardRef(({ result, targetTime }, ref) => {
   return (
-    <dialog className="result-modal">
+    <dialog ref={ref} className="result-modal">
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
@@ -13,6 +15,6 @@ const ResultModal = ({ result, targetTime }) => {
       </form>
     </dialog>
   );
-};
+});
 
 export default ResultModal;
