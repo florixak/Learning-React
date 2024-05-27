8d730d1935a5f52a9bccc396a08702ede8867307
florixak <florixakcz@seznam.cz>
2024-05-27 08:43:36 +0200

    Update

--- 01-starting-project/src/components/Quiz.jsx
+++ 01-starting-project/src/components/Quiz.jsx
@@ -1,8 +1,33 @@
 import { useState } from "react";
 
+import QUESTIONS from "../questions.js";
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active Question</p>;
+  const activeQuestion = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
+    /*setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer]
+    });*/
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestion].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestion].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
