14bba2fb5934af534b94e24ace14990ec637b0b1
florixak <florixakcz@seznam.cz>
2024-06-15 10:29:30 +0200

    Update

--- 01-starting-project/src/components/Header.jsx
+++ 01-starting-project/src/components/Header.jsx
@@ -1,8 +1,13 @@
-import logoImg from "../assets/quiz-logo.png"
+import { log } from '../log.js';
+import logoImg from '../assets/logo.png';
 
 export default function Header() {
-  return <header>
-    <img src={logoImg} alt="Quiz logo" />
-    <h1>ReactQuiz</h1>
-  </header>;
+  log('<Header /> rendered', 1);
+
+  return (
+    <header id="main-header">
+      <img src={logoImg} alt="Magnifying glass analyzing a document" />
+      <h1>React - Behind The Scenes</h1>
+    </header>
+  );
 }
