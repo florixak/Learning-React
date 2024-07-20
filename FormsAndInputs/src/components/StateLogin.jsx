b7dbe4f094e07b1f9a9757508afd1ffc60aa3309
florixak <florixakcz@seznam.cz>
2024-07-20 06:45:24 +0200

    Update StateLogin.jsx

--- FormsAndInputs/src/components/StateLogin.jsx
+++ FormsAndInputs/src/components/StateLogin.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import Input from "./Input";
+import {
+  isEmail,
+  isNotEmpty,
+  hasMinLength,
+} from "../util/validation";
 
 export default function Login() {
   // const [enteredEmail, setEnteredEmail] = useState("");
@@ -15,9 +20,12 @@ export default function Login() {
     password: false,
   });
 
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+  const emailIsInvalid =
+    didEdit.email &&
+    !isEmail(enteredValues.email) &&
+    !isNotEmpty(enteredValues.email);
   const passwordIsInvalid =
-    didEdit.password && enteredValues.password.trim().length < 6;
+    didEdit.password && !hasMinLength(enteredValues.password, 6);
 
   function handleSubmit(event) {
     event.preventDefault();
